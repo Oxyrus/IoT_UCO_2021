56458d021bb452a7d25f544a32bb4057df406e11
Juan G. Velasquez <velasjg@Juans-MacBook-Pro.local>
2021-02-16 18:19:20 -0500

    Added Basic Examples

--- ejemplos/Basic_WebServer/src/main.cpp
+++ ejemplos/Basic_WebServer/src/main.cpp
@@ -1,117 +1,52 @@
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+// Replace with your network credentials
+const char* ssid = "xxxxx";
+const char* password = "xxxxx";
+
+// WebServer Instantiation
+ESP8266WebServer server(80);
+
+//Web Page Variable
+String webPage = "";
  
-const char* ssid = "IoT-UCO";
-const char* password =  "LifeisIoT";
-const char* mqttServer = "tailor.cloudmqtt.com";
-const int mqttPort = 10345;
-const char* mqttUser = "yvnemooa";
-const char* mqttPassword = "lxqM-EuIHmjL";
-const char* TopicSub = "esp";
-const char* TopicPub = "jgv";
+void setup(void){
+  
+  //Build Basic Web Page using HTML
+  webPage += "<h1>AMAZING WEB SERVER</h1>";
+  webPage += "<p><marquee>######.....IoT Rulz!!!.....######</marquee></p>";
+  delay(1000);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-
-/********* Setup wifi ***************************
-   setup wifi connect to wifi with the constants
-   defined up
-   while does not connect print a "."
-   if connect then print the local ip
-************************************************/
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print(F("Connecting to ")) ;
-  Serial.println(ssid);
-
+  //Serial Comunnication and Wifi Connection kick off
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
+  Serial.println("");
 
+  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(F("."));
+    Serial.print(".");
   }
-
   Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-}
-
-/********* MQTT Callback ***************************
-   here is defined the logic to execute after
-   a messages arrives in the desired
-   topic, for this example the variable:
-   TopicSub
-************************************************/
-void callback(char* topic, byte* payload, unsigned int length) {
-
-  //Notify about message arrived 
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
   
-  //Print the message received
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  
-  //Prints with visualization purposes
-  Serial.println();
-  Serial.println("-----------------------");
- 
-}
+  //Web Page handler on root "/" using .send() method from ESP8266WebServer class
+  server.on("/", [](){
+    server.send(200, "text/html", webPage);
+  });
 
-void setup() {
- 
-  //Start Serial Communication
-  Serial.begin(115200);
-  pinMode(D0, INPUT);
-  
-  //Connect to WiFi
-  setup_wifi();
+  //Start Web Server and notify via serial comm.
+  server.begin();
+  Serial.println("HTTP server started");
 
-  //Connect to MQTT Broker
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
- 
-  //MQTT Connection Validation
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
- 
-    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
- 
-      Serial.println("connected");  
- 
-    } else {
- 
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
- 
-    }
-  }
-  
-  //Publish to desired topic and subscribe for messages
-  //client.publish(TopicPub, "Hello from ESP8266");
-  client.subscribe(TopicSub);
- 
 }
  
-void loop() {
-
-  if(digitalRead(D0)==0)
-  {
-    Serial.print("Alarmed!");
-    client.publish(TopicPub, "Alarm!!!");
-    delay(1000);
-  } else {
-    Serial.print("OK");
-    delay(1000);
-    }
-  //MQTT client loop
-  client.loop();
+void loop(void){
+  //Handle client requests to web Server using .handleClient() method from ESP8266WebServer class
+  server.handleClient();
 }
